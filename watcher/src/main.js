64d60bb0ed7e7ae2fd52273d45137799491719ba
vutaka <vutaka0120@gmail.com>
2018-10-10 17:45:07 +0900

    practice watcher

---
+++ watcher/src/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#app' ,
+    data: {
+        list: [],
+        current: '',
+        topics: [
+            {value: 'vue', name: 'Vue.js'},
+            {value: 'jQuery', name: 'jQuery'}
+        ]
+    } ,
+    watch :{
+        current: function(val) {
+            axios.get('https://api.github.com/search/repositories', {
+                params:{q: 'topic:' + val}
+            }).then(function(response) {
+                this.list = response.data.items
+            }.bind(this))
+        }
+    }
+})
