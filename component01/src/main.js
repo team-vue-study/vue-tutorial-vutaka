e0755329eeef3fc5068b4aa10c481ec49406a777
vutaka <vutaka0120@gmail.com>
2018-10-10 18:58:16 +0900

    コンポーネントでリアクティブデータの取り扱い

--- component01/src/main.js
+++ component01/src/main.js
@@ -1,14 +1,18 @@
 const myComponent = {
-    template: '<p>{{message}}</p>',
-    data: function() {
-        return {
-            message: 'hello Vue Component'
-        }
-    }
+    template: '<p>{{val}}</p>',
+    props: ['val']
+}
+const myInputComponent = {
+    template: '<div><input v-model="val"></div>',
+    props: ['val']
 }
 new Vue({
     el: '#app' ,
+    data: {
+        hoge:'hoge'
+    },
     components: {
-        'my-component': myComponent
+        'my-component': myComponent,
+        'my-input-component': myInputComponent
     }
 })
