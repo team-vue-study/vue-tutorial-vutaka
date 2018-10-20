a785338734855e619614ab7a579ca64c574f61c7
vutaka <vutaka0120@gmail.com>
2018-10-20 12:11:28 +0900

    カードを引けるよう機能追加。合計も表示。デバッグ用にディーラーの合計も見えちゃう

--- black-jack/src/main.js
+++ black-jack/src/main.js
@@ -6,7 +6,7 @@ class Card {
 }
 
 const plaingCard = {
-    template: '<p>{{card.mark}} : {{card.num}}</p>',
+    template: '<span>{{card.mark}} : {{card.num}}</span>',
     props: {
         card: Card
     }
@@ -15,12 +15,15 @@ const plaingCard = {
 new Vue({
     el: '#app' ,
     data: {
-        deck: new Array()
+        deck: new Array(),
+        playerCards: new Array(),
+        dealerCards: new Array()
     },
     components: {
         'playing-card': plaingCard
     },
     created: function(){
+        // トランプの山を作成。
         const numArry = [...new Array(13).keys()].map(i => ++i);
         const markArry = ['heart', 'spade', 'daia', 'clover'];
         numArry.forEach(num => {
@@ -28,5 +31,30 @@ new Vue({
                 this.deck.push(new Card(mark, num));
             });
         });
+    },
+    methods:{
+        rand: function() {
+            return Math.floor(Math.random() * this.deck.length);
+        },
+        draw: function(cards) {
+            cards.push(...(this.deck.splice(this.rand(), 1)));
+        },
+        toSumCards: function(cards){
+            let sum = 0;
+            cards.forEach(card => {
+                sum += (card.num > 11 ? 10 : card.num);
+            });
+            return sum;
+        }
+    },
+    computed: {
+        sumPlayerCard: function() {return this.toSumCards(this.playerCards)},
+        sumDealerCard: function() {return this.toSumCards(this.dealerCards)}
+    },
+    mounted: function() {
+        this.draw(this.playerCards);
+        this.draw(this.playerCards);
+        this.draw(this.dealerCards);
+        this.draw(this.dealerCards);
     }
 })
