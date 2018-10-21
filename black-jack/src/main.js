f05fbfaefb1743e98f33e45c98b69c7733ef388f
vutaka <vutaka0120@gmail.com>
2018-10-21 14:43:57 +0900

    だいたいできた

--- black-jack/src/main.js
+++ black-jack/src/main.js
@@ -15,9 +15,18 @@ const plaingCard = {
 new Vue({
     el: '#app' ,
     data: {
+        resultMessage: '',
         deck: new Array(),
-        playerCards: new Array(),
-        dealerCards: new Array()
+        player: {
+            cards: new Array(),
+            stand: false,
+            sum: 0
+        },
+        dealer: {
+            cards: new Array(),
+            stand: false,
+            sum: 0
+        }
     },
     components: {
         'playing-card': plaingCard
@@ -36,26 +45,92 @@ new Vue({
         rand: function() {
             return Math.floor(Math.random() * this.deck.length);
         },
-        hit: function(cards) {
+        hit: function(user) {
+            const cards = user.cards;
             cards.push(...(this.deck.splice(this.rand(), 1)));
+            this.$set(user, 'cards', cards);
+            this.toCalcSum(user);
         },
-        toSumCards: function(cards){
-            let sum = 0;
-            cards.forEach(card => {
-                sum += (card.num > 11 ? 10 : card.num);
+        toCalcSum: function(user){
+            let sum = 0, existAce = false;
+            user.cards.forEach((card)=> {
+                sum += (card.num > 10 ? 10 : card.num);
+                if(card.num == 1) existAce = true;
             });
-            return sum;
+            // Aがあればバーストしない範囲で11になるよう計算する
+            if(existAce && !this.isBust(sum + 10)) {
+                this.$set(user, 'sum', sum + 10);
+            } else {
+                this.$set(user, 'sum', sum);
+            }
+        },
+        stand: function(user) {
+            this.$set(user, 'stand', true);
+        },
+        isStand: function(user) {
+            return user.stand;
+        },
+        isBlackjack: function(value) {
+            return value == 21;
+        },
+        isBust: function(value) {
+            return value > 21;
+        },
+        viewBlackjack: function(value) {
+            return this.isBlackjack(value) ? 'Black Jack!': (this.isBust(value)) ? 'Bust!!!' : value;
+        },
+        checkResult: function() {
+            if(this.isBust(this.player.sum)) {
+                this.resultMessage = 'You Lose...'
+                return;
+            }
+            if(this.isBust(this.dealer.sum)) {
+                this.resultMessage = 'You Win!'
+                return;
+            }
+            // バーストは先に判定するのであとは大きいほうが勝ち
+            if(this.player.sum > this.dealer.sum) {
+                this.resultMessage = 'You Win!'
+            } else {
+                this.resultMessage = 'You Lose...'
+            }
         }
     },
     computed: {
         // アロー関数使うとthisが束縛されないからfunctionとするっきゃない
-        sumPlayerCard: function() {return this.toSumCards(this.playerCards)},
-        sumDealerCard: function() {return this.toSumCards(this.dealerCards)}
+        isStandPlayer: function() {return this.isStand(this.player)},
+        isStandDealer: function() {return this.isStand(this.dealer)},
+        // フィルターからmethodsへのアクセスができない？
+        viewPlayerSum: function() {
+            return this.viewBlackjack(this.player.sum);
+        },
+        viewDealerSum: function() {
+            return this.viewBlackjack(this.dealer.sum);
+        }
+    },
+    // ウォッチャが発火されるのは他のVueイベントがひと段落ついてから。
+    // メソッド実行中には何もされない
+    watch: {
+        // オブジェクト のプロパティは文字列で書かないと反応しない
+        'player.stand' : function() {
+            if(this.isBust(this.player.sum)){
+                this.stand(this.dealer);
+                return;
+            }
+            while(this.dealer.sum < 17) {
+                this.hit(this.dealer);
+            }
+            this.stand(this.dealer);
+        },
+        'dealer.stand' : function() {
+            this.checkResult();
+        }
     },
     mounted: function() {
-        this.hit(this.playerCards);
-        this.hit(this.playerCards);
-        this.hit(this.dealerCards);
-        this.hit(this.dealerCards);
+        this.hit(this.player);
+        this.hit(this.player);
+        this.hit(this.dealer);
+        this.hit(this.dealer);
     }
+
 })
